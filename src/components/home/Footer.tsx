cb1304a6bfaee660d881391288225ef0e74f6b84
ImmortalSul <idofsulaiman@gmail.com>
2025-12-28 22:48:15 +0530

    social links update

--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -27,9 +27,9 @@ export default function Footer() {
             <div className="flex flex-col gap-6">
               <h4 className="font-bold text-gray-400 tracking-wider">COMMUNITY</h4>
               <div className="flex flex-col gap-4 text-sm font-medium text-gray-500">
-                <a href="#" className="hover:text-[#10e291] transition-colors">X (TWITTER)</a>
+                <a href="https://x.com/BlockAi_live" target="_blank" rel="noopener noreferrer" className="hover:text-[#10e291] transition-colors">X (TWITTER)</a>
                 <a href="https://discord.gg/7XR2kafsAT" target="_blank" rel="noopener noreferrer" className="hover:text-[#10e291] transition-colors">DISCORD</a>
-                <a href="#" className="hover:text-[#10e291] transition-colors">TELEGRAM</a>
+                <a href="https://t.me/BlockAiOrg" target="_blank" rel="noopener noreferrer" className="hover:text-[#10e291] transition-colors">TELEGRAM</a>
               </div>
             </div>
 
